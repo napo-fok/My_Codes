11df64354b746b0cd26e4cc1fa98ce93611a2c67
Fok Nap <napo_fok@outlook.com>
2022-01-16 13:03:43 -0800

    Updates to djangoWebApp - Models

--- django_web_app/merchex/listings/admin.py
+++ django_web_app/merchex/listings/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from listings.models import Listing
+
+from listings.models import Band
 
 # Register your models here.
+class BandAdmin(admin.ModelAdmin):
+    list_display=('name','year_formed','genre')
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display=('title', 'band')
+
+
+admin.site.register(Band, BandAdmin)
+admin.site.register(Listing, ListingAdmin)
--- django_web_app/merchex/listings/models.py
+++ django_web_app/merchex/listings/models.py
@@ -1,9 +1,57 @@
+from django.core import validators
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
+from django.db.models.fields.related import ForeignKey
+
 
 # Create your models here.
 
 class Band(models.Model):
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+    class Genre(models.TextChoices):
+        HIP_HOP = 'HH'
+        SYNTH_POP = 'SP'
+        ALTERNATIVE_ROCK = 'AR'
+
+    class type(models.TextChoices):
+        RECORDS = 'REC'
+        CLOTHING = 'CLO'
+        POSTERS = 'POS'
+        MISCELLANEOUS = 'MIS'
+
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2021)])
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+    description = models.fields.CharField(max_length=500, default='Classic Music')
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2021)], default=1915)
+    type = models.fields.CharField(choices=type.choices, max_length=5, default='REC')
+
+
 
 class Listing(models.Model):
+
+    def __str__(self) -> str:
+        return f'{self.title}'
+
+    class type(models.TextChoices):
+        RECORDS = 'REC'
+        CLOTHING = 'CLO'
+        POSTERS = 'POS'
+        MISCELLANEOUS = 'MIS'
+
+
     title = models.fields.CharField(max_length=100)
+    description = models.fields.CharField(max_length=500, default='Pop Music')
+    sold = models.fields.BooleanField(default=False)
+    year = models.fields.IntegerField(validators=[MinValueValidator(1900), MaxValueValidator(2021)], default=1915)
+    type = models.fields.CharField(choices=type.choices, max_length=5, default='MIS')
+
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
+
